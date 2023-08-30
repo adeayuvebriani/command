702ecc4a86c35005fcee55a8b009287b8eb3e92d
adeayuvebriani <143151642+adeayuvebriani@users.noreply.github.com>
2023-08-31 07:50:55 +0800

    Add files via upload

---
+++ EPIC-51pool.sh
@@ -0,0 +1,12 @@
+rem * Replace <YOUR_USERNAME>, <YOUR_PASSWORD> and <WORKER> with the values for your wallet and worker name.
+
+
+rem TT commandline
+./TT-Miner -c epic -P zakyfuadi#qalby:kopipahit@51pool.online:4416
+
+rem TT commandline with alternate coin
+rem ./TT-Miner -c epic -P <YOUR_USERNAME>#<WORKER>:<YOUR_PASSWORD>@51pool.online:4416 -cALT <ALTERNATE_COIN> -PALT <ALTERNATE_INFO>
+pause
+
+
+
